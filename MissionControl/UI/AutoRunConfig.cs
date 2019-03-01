6b126799e14bcc5c22ef4ede71b5b86455a2bcd8
futte111 <ntthyssen@gmail.com>
2019-03-01 17:46:06 +0100

    merge

--- MissionControl/UI/AutoRunConfig.cs
+++ MissionControl/UI/AutoRunConfig.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.IO;
 using System.IO.Ports;
 using System.Linq;
+using Castle.Core.Internal;
 using Gdk;
 using Gtk;
 using MissionControl.Data;
@@ -34,92 +35,81 @@ namespace MissionControl.UI
         LabelledEntryWidget oxidTimeState3;
         LabelledEntryWidget oxidPercentState3;
    
-        private Label headerFuelLabel1;
-        private Label headerFuelLabel2;
-        private Label headerFuelLabel3;
-        private Label headerOxidLabel1;
-        private Label headerOxidLabel2;
-        private Label headerOxidLabel3;
-   
 
         private Button _btnAutoRunConfigSave;
+        
         public AutoRunConfig() : base(WindowType.Toplevel)
         {
             Title = "Auto Sequence Config";
             
-            SetSizeRequest(800, 350);
+            SetSizeRequest(950, 350);
             SetPosition(WindowPosition.Center);
             Console.WriteLine("hello autoconfig");
-            _btnAutoRunConfigSave = new Button{Label = "Save", WidthRequest = 50, HeightRequest = 40};
-            headerFuelLabel1 = new Label("Fuel State 1");
-            headerFuelLabel2 = new Label("Fuel State 2");
-            headerFuelLabel3= new Label("Fuel State 3");
-            headerOxidLabel1 = new Label("Oxid State 1");
-            headerOxidLabel2 = new Label("Oxid State 2");
-            headerOxidLabel3= new Label("Oxid State 3");
+            _btnAutoRunConfigSave = new Button{Label = "Save", WidthRequest = 80, HeightRequest = 40};
             startDelay = new LabelledEntryWidget {
                 LabelText = "Start Delay:",
                 EntryText =  "",
             };
             fuelTimeState1 = new LabelledEntryWidget
             {
-                LabelText =   "Time:",
+                LabelText =   "Time Fuel State1:",
                 EntryText =  "",
-                
+                    
            
             };
+            
             fuelPercentState1 = new LabelledEntryWidget
             {
-                LabelText = "Position:",
+                LabelText = "Position Fuel State1:",
                 EntryText = "",
                 
             };
             
             fuelTimeState2 = new LabelledEntryWidget
             {
-                LabelText = "Time:",
+                LabelText = "Time Fuel State2:",
                 EntryText = "",
                 
             };
             
             fuelPercentState2 = new LabelledEntryWidget
             {
-                LabelText = "Position:",
+                LabelText = "Position Fuel State2:",
                 EntryText = "",
                 
             };
             
             fuelTimeState3 = new LabelledEntryWidget
             {
-                LabelText = "Time:",
+                LabelText = "Time Fuel State3:",
                 EntryText = "",
                 
             };
             
             fuelPercentState3 = new LabelledEntryWidget
             {
-                LabelText = "Position:",
+                LabelText = "Position Fuel State3:",
                 EntryText = "",
                
             };
             
              oxidTimeState1 = new LabelledEntryWidget
             {
-                LabelText =   "Time:",
+                LabelText =   "Time Oxid State1:",
                 EntryText =  "",
                 
            
             };
             oxidPercentState1 = new LabelledEntryWidget
             {
-                LabelText = "Position:",
+                LabelText = "Position Oxid State1:",
                 EntryText = "",
                 
             };
             
             oxidTimeState2 = new LabelledEntryWidget()
             {
-                LabelText = "Time:",
+                LabelText = "Time Oxid State2:",
                 EntryText = "",
                 
                 
@@ -127,20 +117,20 @@ namespace MissionControl.UI
             
             oxidPercentState2 = new LabelledEntryWidget
             {
-                LabelText = "Position:",
+                LabelText = "Position Oxid State2:",
                 EntryText = "",
                 
             };
             
             oxidTimeState3 = new LabelledEntryWidget
             {
-                LabelText = "Time:",
+                LabelText = "Time Oxid State1:",
                 EntryText = "",
             };
             
             oxidPercentState3 = new LabelledEntryWidget
             {
-                LabelText = "Position:",
+                LabelText = "Position Oxid State1:",
                 EntryText = "",
             };
             
@@ -154,9 +144,11 @@ namespace MissionControl.UI
 
             endTime = new LabelledEntryWidget
             {
-                LabelText = "End Time",
+                LabelText = "End Time:",
                 EntryText = ""
             };
+            
+            
 
             VBox container = new VBox(false , 50);
             HBox headerTop = new HBox(false, 0);
@@ -187,23 +179,23 @@ namespace MissionControl.UI
             ignitionBox.PackStart(ignitionTime, false, false, 0);
             endTimeBox.PackStart(endTime, false, false,0);
         
-            fuelState1.PackStart(headerFuelLabel1, true,false,0);
+           // fuelState1.PackStart(headerFuelLabel1, false,false,0);
             fuelState1.PackStart(fuelTimeState1, false , false,0);
             fuelState1.PackEnd(fuelPercentState1, false ,false,0);
-            fuelState2.PackStart(headerFuelLabel2, false, false,0 );
+            //fuelState2.PackStart(headerFuelLabel2, false, false,0 );
             fuelState2.PackStart(fuelTimeState2,false,false,0);
             fuelState2.PackEnd(fuelPercentState2,false,false,0);
-            fuelState3.PackStart(headerFuelLabel3, false, false,0);
+            //fuelState3.PackStart(headerFuelLabel3, false, false,0);
             fuelState3.PackStart(fuelTimeState3,false,false,0);
             fuelState3.PackEnd(fuelPercentState3, false,false,0);
-            oxidState1.PackStart(headerOxidLabel1, false, false, 0);
+            //oxidState1.PackStart(headerOxidLabel1, false, false, 0);
             oxidState1.PackStart(oxidTimeState1, false, false, 0);
             oxidState1.PackEnd(oxidPercentState1, false, false, 0);
             
-            oxidState2.PackStart(headerOxidLabel2, false, false, 0);
+            //oxidState2.PackStart(headerOxidLabel2, false, false, 0);
             oxidState2.PackStart(oxidTimeState2, false, false ,0);
             oxidState2.PackEnd(oxidPercentState2, false , false , 0);
-            oxidState3.PackStart(headerOxidLabel3, false, false, 0);
+            //oxidState3.PackStart(headerOxidLabel3, false, false, 0);
             oxidState3.PackStart(oxidTimeState3, false, false, 0);
             oxidState3.PackStart(oxidPercentState3, false ,false, 0);
             bottom.PackStart(oxidState1, false, false, 0);
@@ -215,12 +207,10 @@ namespace MissionControl.UI
             ShowAll();
         }
         
-        
-        
-        
         public void getTimeings(object sender, EventArgs e)
         {
-           
+
+            String errorMsg = "";
             List<LabelledEntryWidget> entryValues = new List<LabelledEntryWidget>
             
             {
@@ -240,17 +230,87 @@ namespace MissionControl.UI
                 oxidPercentState3,
                 endTime
             };
-            int[] intList = new int[15];
+            
+            List<LabelledEntryWidget> entryValuesPosition = new List<LabelledEntryWidget>
+            
+            {
+                fuelPercentState1,
+                fuelPercentState2,
+                fuelPercentState3,
+                oxidPercentState1,
+                oxidPercentState2,
+                oxidPercentState3,
+                
+            };
+            ushort[] intList = new ushort[15];
             int counter = 0;
             foreach (LabelledEntryWidget label in entryValues)
             {
-                Int32.TryParse(label.EntryText, out intList[counter++]);
-                Console.WriteLine(intList[counter++].GetType());
-                counter++;
+                ushort.TryParse(label.EntryText, out intList[counter]);
+               
             }
 
+            
+            for(int i = 0; i < entryValues.Count; i++)
+            {                
+                if (entryValues[i].EntryText.IsNullOrEmpty())
+                {
+                    errorMsg += "-" + entryValues[i].LabelText +" Must not be Empty\n";
+                    
+                }
+
+            
+
+            }
+            if (Int32.Parse(fuelTimeState1.EntryText) > Int32.Parse(fuelTimeState2.EntryText))
+            {
+                errorMsg += fuelTimeState2.LabelText + " Must be higher than " + fuelTimeState1.LabelText + "\n";
+            }
                 
+            if (Int32.Parse(fuelTimeState2.EntryText) > Int32.Parse(fuelTimeState3.EntryText))
+            {
+                errorMsg += fuelTimeState3.LabelText + " Must be higher than " + fuelTimeState2.LabelText + "\n";
+
+            }
+            
+            if (Int32.Parse(oxidTimeState1.EntryText) > Int32.Parse(oxidTimeState2.EntryText))
+            {
+                errorMsg += oxidTimeState2.LabelText + " Must be higher than " + oxidTimeState1.LabelText + "\n";
+            }
+                
+            if (Int32.Parse(oxidTimeState2.EntryText) > Int32.Parse(oxidTimeState3.EntryText))
+            {
+                errorMsg += oxidTimeState3.LabelText + " Must be higher than " + oxidTimeState2.LabelText + "\n";
+
+            }
+
+            int[] positionValues = new int[6];
+            for (int i = 0; i < entryValuesPosition.Count; i++)
+            {
+                Int32.TryParse(entryValuesPosition[i].EntryText, out positionValues[i]);
+                if (!Enumerable.Range(0,100).Contains(positionValues[i]))
+                {
+                    errorMsg +=  entryValuesPosition[i].LabelText + " Values must between 0 - 100 in \n";
+                }
+            }
+
+            if (!errorMsg.IsNullOrEmpty())
+            {
+                MessageDialog errorDialog = new MessageDialog(this, 
+                    DialogFlags.DestroyWithParent, 
+                    MessageType.Error, 
+                    ButtonsType.Close, 
+                    errorMsg
+                );
+                errorDialog.Run();
+                errorDialog.Destroy(); 
+
+                
+            }
+            
         }
+       
+       
         
         
     }
